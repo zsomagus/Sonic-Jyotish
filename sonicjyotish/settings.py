d79c7e3f1fe2b61b73fe125045401b1deecb771f
zsomagus <zsomagus@gmail.com>
2025-07-21 21:38:36 +0200

    2140

--- sonicjyotish/settings.py
+++ sonicjyotish/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sonicjyotish',  # ← saját appod neve
+    'sonicjyotish.apps.SonicjyotishConfig',  # ← saját appod neve
 ]
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
