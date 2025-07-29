c6559c19f17de4b0faa388d9337020b3c4cb03d4
zsomagus <zsomagus@gmail.com>
2025-07-29 17:52:37 +0200

    temlaates

--- sonicjyotish/settings.py
+++ sonicjyotish/settings.py
@@ -52,7 +52,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [os.path.join(BASE_DIR, "view", / "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
